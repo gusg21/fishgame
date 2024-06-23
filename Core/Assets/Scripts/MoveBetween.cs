fa95ab323cfbe44f8e369d4ccf3b3dc66f4544e3
James <james.mckibbin@mymail.champlain.edu>
2024-06-22 22:35:20 -0400

    started fishing UI

---
+++ Core/Assets/Scripts/MoveBetween.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBetween : MonoBehaviour
+{
+    [SerializeField] private bool _moveInOnAwake = false;
+    [SerializeField] private bool _moveInOnEnable = false;
+    [SerializeField] private bool _moveToOutPosOnAwake = false;
+    [SerializeField] private float _moveTightness = 0.2f;
+    [SerializeField] private Vector3 _posOut;
+    [SerializeField] private Vector3 _posIn;
+    private Vector3 _currentTarget = Vector3.zero;
+    private bool _moving;
+
+    private void Awake()
+    {
+        if (_moveInOnAwake)
+            MoveIn();
+
+        if (_moveToOutPosOnAwake)
+            transform.position = _posOut;
+    }
+
+    private void OnEnable()
+    {
+        if (_moveInOnEnable) 
+            MoveIn();
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.localPosition, _currentTarget) < 0.01f)
+        {
+            _moving = false;
+        }
+        if (_moving)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, _currentTarget, _moveTightness);
+        }
+    }
+
+    private void Move(Vector3 from, Vector3 to)
+    {
+        transform.localPosition = from;
+        _currentTarget = to;
+        _moving = true;
+    }
+
+    public void MoveIn() => Move(_posOut, _posIn);
+    public void MoveOut() => Move(_posIn, _posOut);
+
+    public void MoveInRelative() => Move(_posOut + transform.localPosition, _posIn + transform.localPosition);
+    public void MoveOutRelative() => Move(_posIn + transform.localPosition, _posOut + transform.localPosition);
+    
+
+}
